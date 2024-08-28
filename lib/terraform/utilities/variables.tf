c94cbd3ffc13e630fd433e9f9a6f91f45ba1e2d2
j2joi <julian.ortiz@oracle.com>
2024-08-28 14:43:09 -0400

    feat: RMS stack to parse Datasources only. Integrated with main-discovery.py
    Signed-off-by: j2joi <julian.ortiz@oracle.com>

---
+++ lib/terraform/utilities/variables.tf
@@ -0,0 +1,63 @@
+# Copyright (c) 2024 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+# Common
+variable "region" { type = string }
+variable "wlsserver_pools" { type = any }
+
+# Connection
+variable "bastion_host" { type = string }
+variable "bastion_user" { type = string }
+
+variable "ssh_private_key" {
+  type      = string
+  sensitive = true
+}
+
+#TODO: JOI: retire operator
+#variable "operator_host" { type = string }
+#variable "operator_user" { type = string }
+
+## OCIR
+#variable "ocir_email_address" { type = string }
+#variable "ocir_secret_id" { type = string }
+#variable "ocir_secret_name" { type = string }
+#variable "ocir_secret_namespace" { type = string }
+#variable "ocir_username" { type = string }
+
+# Node readiness check, drain
+variable "await_node_readiness" { type = string }
+#variable "expected_drain_count" { type = number }
+variable "expected_node_count" { type = number }
+variable "restore_wls_archives" { type = string }
+variable "text_to_replace_in_config" {
+  type = any
+  default= {}
+}
+variable "wls_domain_path" {
+  type = string
+  default = "none"
+}
+#variable "wlsserver_instance_private_ips" { type = any }
+#variable "worker_drain_ignore_daemonsets" { type = bool }
+#variable "worker_drain_delete_local_data" { type = bool }
+#variable "worker_drain_timeout_seconds" { type = number }
+variable "user" {
+  type = string
+}
+variable "group" {
+  type = string
+}
+
+#TODO : JOI : pre-release delete default for user_id and group_id
+variable "user_id" {
+  type = number
+}
+
+variable "group_id" {
+  type = number
+}
+
+#variable "bucket_name" {type = string}
+
+variable "resource_name_prefix" {type = string}
---
+++ oci/iac2/examples/rms/wls-datasources-only/variables-resource-datasource.tf
@@ -0,0 +1,130 @@
+# Copyright (c) 2024 Oracle Corporation and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
+
+# This set of variables are used to capture datasource changes in RMS UI.
+variable "update_any_ds" {
+  type = bool
+  default = false
+}
+variable "ds_1" {
+  type = string
+  default = ""
+}
+variable "ds_2" {
+  type = string
+  default = ""
+}
+variable "ds_3" {
+  type = string
+  default = ""
+}
+variable "ds_4" {
+  type = string
+  default = ""
+}
+variable "ds_5" {
+  type = string
+  default = ""
+}
+variable "ds_6" {
+  type = string
+  default = ""
+}
+variable "ds_7" {
+  type = string
+  default = ""
+}
+variable "ds_8" {
+  type = string
+  default = ""
+}
+variable "ds_9" {
+  type = string
+  default = ""
+}
+variable "ds_10" {
+  type = string
+  default = ""
+}
+
+variable "editable_ds_1" {
+  type=string
+  default = "n/a"
+}
+variable "editable_ds_2" {
+  type=string
+  default = ""
+}
+variable "editable_ds_3" {
+  type=string
+  default = ""
+}
+variable "editable_ds_4" {
+  type=string
+  default = ""
+}
+variable "editable_ds_5" {
+  type=string
+  default = ""
+}
+variable "editable_ds_6" {
+  type=string
+  default = ""
+}
+variable "editable_ds_7" {
+  type=string
+  default = ""
+}
+variable "editable_ds_8" {
+  type=string
+  default = ""
+}
+variable "editable_ds_9" {
+  type=string
+  default = ""
+}
+variable "editable_ds_10" {
+  type=string
+  default = ""
+}
+
+variable "update_ds_1" {
+  type=bool
+  default = false
+}
+variable "update_ds_2" {
+  type=bool
+  default = false
+}
+variable "update_ds_3" {
+  type=bool
+  default = false
+}
+variable "update_ds_4" {
+  type=bool
+  default = false
+}
+variable "update_ds_5" {
+  type=bool
+  default = false
+}
+variable "update_ds_6" {
+  type=bool
+  default = false
+}
+variable "update_ds_7" {
+  type=bool
+  default = false
+}
+variable "update_ds_8" {
+  type=bool
+  default = false
+}
+variable "update_ds_9" {
+  type=bool
+  default = false
+}
+variable "update_ds_10" {
+  type=bool
+  default = false
+}
