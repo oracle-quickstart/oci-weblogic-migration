674e5c28b6ea903ec4605346857702139ae8a891
j2joi <julian.ortiz@oracle.com>
2025-01-21 19:08:39 -0500

    fix: cloud-init template is not parsed when there is no datasources
    Signed-off-by: j2joi <julian.ortiz@oracle.com>

--- oci/rm/iac/modules/wlsservers/cloudinit.tf
+++ oci/rm/iac/modules/wlsservers/cloudinit.tf
@@ -185,7 +185,7 @@ data "cloudinit_config" "wlsservers" {
 
    # Write Python files to perform database related task
    dynamic "part" {
-     for_each = each.value.disable_default_cloud_init && length(var.wls_datasources_config) > 0 ? [] : [1]
+     for_each = each.value.disable_default_cloud_init && try(length(var.wls_datasources_config),0) > 0 ? [] : [1]
      content {
        content_type = "text/cloud-config"
        content = jsonencode({
@@ -268,8 +268,8 @@ data "cloudinit_config" "wlsservers" {
 
   # Update Datasources
   dynamic "part" {
-#     for_each = each.value.disable_default_cloud_init && length(var.wls_datasources_config) > 0? [] : [1]
-    for_each = each.value.disable_default_cloud_init ? [] : [1]
+    for_each = each.value.disable_default_cloud_init && try(length(var.wls_datasources_config),0) > 0? [] : [1]
+#     for_each = each.value.disable_default_cloud_init ? [] : [1]
     content {
       content_type = "text/x-shellscript"
       content      = templatefile("${path.module}/templates/cloudinit-wls-update-jdbc-datasources.tpl.sh", {
