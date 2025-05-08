f8b24a8f43f7692d6ca71a25204c12c05efddecb
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-05-08 15:12:05 +0530

    topic_mb_JCS-14717 update schema.yaml.mustache

--- oci/rm/variables-servers.tf
+++ oci/rm/variables-servers.tf
@@ -37,7 +37,7 @@ variable "wlsserver_node_labels" {
 variable "wlsserver_image_type" {
   type        = string
   description = "Type of image used for provisioning. Image type must be Oracle WebLogic Server BYOL or Oracle WebLogic Server UCM"
-  default     = "Oracle WebLogic Server Enterprise Edition UCM Image"
+  default     = "Oracle Weblogic Server UCM Image"
   validation {
     condition     = contains(["Oracle WebLogic Server BYOL Image", "Oracle WebLogic Server UCM Image", "custom", "platform"], var.wlsserver_image_type)
     error_message = "WLSC-ERROR: Allowed values for Weblogic Edition are 'Oracle WebLogic Server BYOL' or 'Oracle Weblogic Suite UCM' or 'Oracle WebLogic Server Enterprise Edition UCM' "
