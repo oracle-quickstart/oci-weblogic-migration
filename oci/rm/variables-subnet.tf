01371f70546a52d1270289e34a1a290c88d1e8d5
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-05-08 19:29:38 +0530

    topic_mb_JCS-14717 update schema.yaml.mustache

--- oci/rm/variables-subnet.tf
+++ oci/rm/variables-subnet.tf
@@ -9,7 +9,7 @@ variable "wlsserver_subnet_create" { default = false }
 variable "bastion_subnet_newbits" { default = 13 }
 variable "int_lb_subnet_newbits" { default = 11 }
 variable "pub_lb_subnet_newbits" { default = 11 }
-#variable "wlsserver_subnet_newbits" { default = 2 }
+variable "wlsserver_subnet_newbits" { default = 2 }
 
 variable "wlsserver_subnet_cidr" {
   type        = string
