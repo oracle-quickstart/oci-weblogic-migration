8a41000eb3e3c8dfa954951c9ffc75a77b5bfacd
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-05-08 22:45:30 +0530

    topic_mb_JCS-14717 update schema.yaml.mustache

--- oci/rm/iac/module-subnet.tf
+++ oci/rm/iac/module-subnet.tf
@@ -1,5 +1,12 @@
 locals {
   network_compartment_id       = var.network_compartment_id == "" ? var.compartment_ocid : var.network_compartment_id
+
+  # Map of configured subnets to specified/generated dns_label when enabled
+  # If `assign_dns = true`, use dns_label for subnet if specified or first 2 characters of subnet key
+  subnet_dns_labels = { for k, v in var.subnets :
+    k => coalesce(lookup(v, "dns_label", null), substr(k, 0, 2))
+    if var.assign_dns
+  }
 }
 
   /* Create back end  private subnet for wls */
@@ -11,7 +18,7 @@ module "network-wls-private-subnet" {
   #This is to prevent Terraform from resetting the route table on reapply. Peering module will set a new route table
   route_table_id     = var.nat_route_table_id
   subnet_name        = format("wlsservers-%v", var.state_id)
-  dns_label          = format("wlsservers-%v", substr(strrev(var.state_id), 0, 7))
+  dns_label          = lookup(local.subnet_dns_labels, "wlsservers", null)
   cidr_block         = var.wlsserver_subnet_cidr
   prohibit_public_ip = true
 
