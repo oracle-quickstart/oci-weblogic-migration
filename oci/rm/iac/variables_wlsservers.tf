1163ab0379c03384d12859dc6336d3acd1f3d5b3
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-05-09 20:54:14 +0530

    topic_mb_JCS-14717 update schema.yaml.mustache

--- oci/rm/iac/variables_wlsservers.tf
+++ oci/rm/iac/variables_wlsservers.tf
@@ -43,7 +43,7 @@ variable "wlsserver_pools" {
 #
 
 variable "wlsserver_block_volume_type" {
-  default     = "iscsi"
+  default     = "paravirtualized"
   description = "Default block volume attachment type for Instance Configurations when unspecified on a pool."
   type        = string
   validation {
