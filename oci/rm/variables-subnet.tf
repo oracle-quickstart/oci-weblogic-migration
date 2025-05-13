cea464a1049afd4b29bad288cfd86f880a40b244
Chintamani Bhat <chintamani.bhat@oracle.com>
2025-05-13 23:50:05 +0530

    test

--- oci/rm/variables-subnet.tf
+++ oci/rm/variables-subnet.tf
@@ -6,7 +6,6 @@ variable "int_lb_subnet_create" { default = false }
 variable "pub_lb_subnet_create" { default = true }
 variable "wlsserver_subnet_create" { default = true }
 
-variable "bastion_subnet_newbits" { default = 13 }
 variable "int_lb_subnet_newbits" { default = 11 }
 variable "pub_lb_subnet_newbits" { default = 11 }
 variable "wlsserver_subnet_newbits" { default = 2 }
