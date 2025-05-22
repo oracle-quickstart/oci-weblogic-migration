6cd4c32f681106340422f3915ed79f7b2901299e
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-05-22 17:54:46 +0530

    topic_mb_JCS-14842_vcn_peering

--- oci/rm/main.tf
+++ oci/rm/main.tf
@@ -11,32 +11,33 @@ locals {
   }] : []
   domain_type         = try(local.wls_data["resources"]["JDBCSystemResource"], {}) != {} ? "jrf" : "non-jrf"
   unique_conn_strings = local.domain_type == "jrf" ? length(distinct([for owner in local.wls_data.resources.JDBCSystemResource : owner.JdbcResource.JDBCDriverParams.URL])) : 0
-
+  datasources_contain_atp = anytrue([for _, v in local.datasources : v.is_atp])
+  datasources_contain_oci_db = anytrue([for _, v in local.datasources : v.is_oci_db])
   // Criteria for VCN peering:
   // 1. Only when both WLS VCN name is provided (wls_vcn_name) and DB VCN ID is provided (either oci_db_existing_vcn_id or atp_db_existing_vcn_id)
   // 2. or when both WLS VCN ID is provided (wls_existing_vcn_id) and DB VCN ID is provided (either oci_db_existing_vcn_id or atp_db_existing_vcn_id) and they are different IDs,
   // and not using existing subnets (local.use_existing_subnets)
 
-  db_strategy_is_oci_db         = local.domain_type == "jrf" ? local.unique_conn_strings > 0 && var.db_strategy_0 == "Database System" : false
-  db_strategy_is_atp_db         = local.domain_type == "jrf" ? local.unique_conn_strings > 0 && var.db_strategy_0 == "Autonomous Transaction Processing Database" : false
+  db_strategy_is_oci_db         = local.unique_conn_strings > 0 ? local.datasources_contain_oci_db : false
+  db_strategy_is_atp_db         = local.unique_conn_strings > 0 ? local.datasources_contain_atp : false
   network_compartment_id        = var.network_compartment_id == "" ? var.compartment_ocid : var.network_compartment_id
-  is_oci_db                     = local.db_strategy_is_oci_db ? trimspace(var.oci_db_dbsystem_id_0) == "" ? false : true : false
-  oci_db_compartment_id         = local.db_strategy_is_oci_db ? (var.oci_db_compartment_id_0 == "" ? local.network_compartment_id : var.oci_db_compartment_id_0) : ""
-  oci_db_network_compartment_id = local.db_strategy_is_oci_db ? (local.is_oci_db && var.oci_db_network_compartment_id_0 == "" ? var.oci_db_compartment_id_0 : var.oci_db_network_compartment_id_0) : ""
+  #is_oci_db                     = local.db_strategy_is_oci_db ? trimspace(var.oci_db_dbsystem_id_0) == "" ? false : true : false
+  oci_db_compartment_id         = local.db_strategy_is_oci_db ? local.oci_db_compartment_id_0 : ""
+  oci_db_network_compartment_id = local.db_strategy_is_oci_db ? local.oci_db_network_compartment_id_0 : ""
 
-  is_atp_db                     = local.db_strategy_is_atp_db ? trimspace(var.atp_db_id_0) != "" : false
-  #is_atp_with_private_endpoints = local.is_atp_db && (length(data.oci_database_autonomous_database.atp_db) != 0 ? data.oci_database_autonomous_database.atp_db[0].subnet_id != null : false)
-  atp_db_network_compartment_id = local.db_strategy_is_atp_db ? (var.atp_db_uses_private_endpoint_0 && var.atp_db_network_compartment_id_0 == "" ? var.atp_db_compartment_id_0 : var.atp_db_network_compartment_id_0) : ""
+  #is_atp_db                     = local.db_strategy_is_atp_db ? trimspace(var.atp_db_id_0) != "" : false
+  #is_atp_with_private_endpoints = local.is_atp_db_0 && (length(data.oci_database_autonomous_database.atp_db) != 0 ? data.oci_database_autonomous_database.atp_db[0].subnet_id != null : false)
+  atp_db_network_compartment_id = local.db_strategy_is_atp_db && local.is_atp_with_private_endpoints_0 ? local.atp_db_network_compartment_id_0 : ""
 
 
-  db_network_compartment_id = local.db_strategy_is_atp_db ? local.atp_db_network_compartment_id : local.oci_db_network_compartment_id
-  db_existing_vcn_id  = local.db_strategy_is_atp_db ||  local.db_strategy_is_oci_db ? ((local.is_atp_db ? (var.atp_db_uses_private_endpoint_0 ? var.atp_db_existing_vcn_id_0 : "") : ( local.is_oci_db ? var.oci_db_existing_vcn_id_0 : "" ))) : ""
+  db_network_compartment_id = local.db_strategy_is_atp_db ? local.atp_db_network_compartment_id : (local.db_strategy_is_oci_db ? local.oci_db_network_compartment_id : "")
+  db_existing_vcn_id  = local.db_strategy_is_atp_db ||  local.db_strategy_is_oci_db ? ((local.is_atp_db_0 ? (local.is_atp_with_private_endpoints_0 ? local.atp_db_existing_vcn_id_0 : "") : ( local.is_oci_db_0 ? local.oci_db_existing_vcn_id_0 : "" ))) : ""
 
-  new_vcn_and_oci_db                    = local.db_strategy_is_oci_db ? (var.create_vcn && local.is_oci_db && var.oci_db_existing_vcn_id_0 != "") : false
-  existing_vcn_and_oci_db_different_vcn = local.db_strategy_is_oci_db ? (var.vcn_id != "" && var.oci_db_existing_vcn_id_0 != "" && var.vcn_id != var.oci_db_existing_vcn_id_0) : false
+  new_vcn_and_oci_db                    = local.db_strategy_is_oci_db ? (var.create_vcn && local.is_oci_db_0 && local.oci_db_existing_vcn_id_0 != "") : false
+  existing_vcn_and_oci_db_different_vcn = local.db_strategy_is_oci_db ? (var.vcn_id != "" && local.oci_db_existing_vcn_id_0 != "" && var.vcn_id != local.oci_db_existing_vcn_id_0) : false
 
-  new_vcn_and_atp_db_private_endpoint                    = local.db_strategy_is_atp_db ? (var.create_vcn && var.atp_db_uses_private_endpoint_0 && var.atp_db_existing_vcn_id_0 != "") : false
-  existing_vcn_and_atp_db_private_endpoint_different_vcn = local.db_strategy_is_atp_db ? (var.vcn_id != "" && var.atp_db_uses_private_endpoint_0 && var.atp_db_existing_vcn_id_0 != "" && var.vcn_id != var.atp_db_existing_vcn_id_0) : false
+  new_vcn_and_atp_db_private_endpoint                    = local.db_strategy_is_atp_db ? (var.create_vcn && local.is_atp_with_private_endpoints_0 && local.atp_db_existing_vcn_id_0 != "") : false
+  existing_vcn_and_atp_db_private_endpoint_different_vcn = local.db_strategy_is_atp_db ? (var.vcn_id != "" && local.is_atp_with_private_endpoints_0 && local.atp_db_existing_vcn_id_0 != "" && var.vcn_id != local.atp_db_existing_vcn_id_0) : false
 
   is_vcn_peering = local.db_strategy_is_atp_db ||  local.db_strategy_is_oci_db ? (local.new_vcn_and_oci_db || local.new_vcn_and_atp_db_private_endpoint || local.existing_vcn_and_oci_db_different_vcn || local.existing_vcn_and_atp_db_private_endpoint_different_vcn) : false
 }
