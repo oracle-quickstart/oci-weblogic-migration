5031617443729cda1d40fbd16415e2b235d3b5fe
Mahuwa-Barman <mahuwa.barman@oracle.com>
2025-06-19 20:22:30 +0530

    topic_mb_JCS-14872_ATP_Bug

--- oci/rm/output.tf
+++ oci/rm/output.tf
@@ -30,7 +30,6 @@ output "load_balancer_ip" {
   value = try(element(flatten(module.wls.wls_loadbalancer_ip), 0).ip_address, "")
 }
 
-# value to be added later when vcn peering support is added, and the variable is defined
 output "is_vcn_peered" {
   value = local.is_vcn_peering
 }
