863f022a1e12136c9a20142eaf34ae95eaff0a4b
manvigupta <manvi.g.gupta@oracle.com>
2025-07-15 02:02:30 +0530

    Added changes for cleaning schema.yaml & for adding the right output in application information

--- oci/rm/variables-loadbalancer.tf
+++ oci/rm/variables-loadbalancer.tf
@@ -2,7 +2,7 @@
 
 variable "create_demo_certificate" {default = false}
 variable "load_balancer_shape" {
-  default = "100Mbps"  #TODO: JOI : remove and set to flexible
+  default = "flexible"  #TODO: JOI : remove and set to flexible
 }
 variable "lb_max_bandwidth" {
   default = "100"
@@ -17,8 +17,4 @@ variable "existing_load_balancer_id" {
 variable "custom_backends" {
   type=list(string)
   default = []
-}
-
-variable "use_existing_loadbalancer" {
-  default = false
 }
