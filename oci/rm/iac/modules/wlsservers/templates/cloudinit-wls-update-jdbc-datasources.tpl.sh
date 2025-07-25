d6ecde4f1f80527821ba3c565507dbf3c631ccc6
Siddharth Sahu <sysahu@dhcp-10-120-141-182.wifi.oracle.com>
2025-07-25 18:25:18 +0530

    Made changes as mentioned in the comments

--- oci/rm/iac/modules/wlsservers/templates/cloudinit-wls-update-jdbc-datasources.tpl.sh
+++ oci/rm/iac/modules/wlsservers/templates/cloudinit-wls-update-jdbc-datasources.tpl.sh
@@ -44,37 +44,32 @@ fi
 cd "${domain_home}/config/jdbc" || (echo "Failed to cd to ${domain_home}/config/jdbc" | log >> $log_file ; exit 1)
 
 
-%{ for jdbc_string in datasources }
-
-  echo "Datasource: ${jdbc_string.index}" | log >> $log_file
-
+%{ for config_key, jdbc_string in datasources }
   is_atp=${jdbc_string.is_atp}
   is_oci_db=${jdbc_string.is_oci_db}
   is_custom_jdbc=${jdbc_string.custom_jdbc}
   on_prem_jdbc_string="${jdbc_string.on_prem}"
   oci_jdbc_string="${jdbc_string.oci}"
 
+     #Opening port 1522 in the subnet of the selected Autonomous Transaction Processing (ATP) Database, if the checkbox is checked.
+  if [ "$is_admin_instance" = "true" ] && [ "${jdbc_string.existing_vcn_add_seclist}" = "true" ]; then
+      output=$(python3 /opt/scripts/open_db_port.py "${jdbc_string.db_port}" "${jdbc_string.db_network_compartment_id}" "${jdbc_string.db_existing_vcn_id}" "${jdbc_string.db_subnet_id}" 2>&1)
+      exit_code=$?
+      echo "Executed script to open ingress port ${jdbc_string.db_port} in db subnet ${jdbc_string.db_subnet_id} with exit code [$exit_code]" | log >> $log_file
+      echo "$output" | log >> $log_file
+      if [ $exit_code -ne 0 ]; then
+          echo "Error executing the script to open ingress port ${jdbc_string.db_port} in db subnet ${jdbc_string.oci_subnet_id}" | log >> $log_file
+          exit 1
+      fi
+  fi
+
   if [ $is_atp == "true" ]; then
-
-
-    #Opening port 1522 in the subnet of the selected Autonomous Transaction Processing (ATP) Database, if the checkbox is checked.
-    if [ "$is_admin_instance" = "true" ] && [ "${jdbc_string.open_atp_port}" = "true" ]; then
-        output=$(python3 /opt/scripts/open_db_port.py "1522" "${jdbc_string.atp_network_comp}" "${jdbc_string.atp_vcn_id}" "${jdbc_string.atp_subnet_id}" 2>&1)
-        exit_code=$?
-        echo "Executed script to open ingress port 1522 in db subnet with exit code [$exit_code]" | log >> $log_file
-        echo "$output" | log >> $log_file
-        if [ $exit_code -ne 0 ]; then
-            echo "Error executing the script to open ingress port 1522 in db subnet, with exit code $exit_code. Output: $output" | log >> $log_file
-            exit 1
-        fi
-    fi
-
     #Wallets will be placed in /u01/oracle/wallet/private/<ocid>
     wallet_location=${domain_home}/wlsdeploy/wallet/private/${jdbc_string.db_id}
     echo "<cloud-init><jdbc-datasources><init> creating wallet path $wallet_location" | log >> $log_file
     output=$(sudo -E -u ${user} mkdir -p "$wallet_location")
     echo $output | log >> $log_file
-    atp_wallet_password=$(python3 -c'import sys; sys.path.append("/opt/scripts"); import atp_db_util; wallet_password = atp_db_util.get_md5_hash("${jdbc_string.db_id}" + ":" + "${jdbc_string.atp_name}") + "Z%%"; print(wallet_password)')
+    atp_wallet_password=$(python3 -c'import sys; sys.path.append("/opt/scripts"); import atp_db_util; wallet_password = atp_db_util.get_md5_hash("${jdbc_string.db_id}" + ":" + "${jdbc_string.atp_db.db_name}") + "Z%%"; print(wallet_password)')
     wallet_pass_exit_code=$?
     download=$(sudo -E -u ${user} echo "$${atp_wallet_password}" | python3 /opt/scripts/atp_db_util.py ${jdbc_string.db_id} "$wallet_location" 2>&1 )
     download_exit_code=$?
@@ -95,7 +90,7 @@ cd "${domain_home}/config/jdbc" || (echo "Failed to cd to ${domain_home}/config/
         fi
     done
     # Find if jspconfig files exist and replace jdbc string if found by this database
-    connection_url="jdbc:oracle:thin:@${jdbc_string.atp_name}_${jdbc_string.atp_level}?TNS_ADMIN=$wallet_location"
+    connection_url="jdbc:oracle:thin:@${jdbc_string.atp_db.db_name}_${jdbc_string.atp_db.db_level}?TNS_ADMIN=$wallet_location"
     output=$(sudo -E -u ${user} grep --include=\*.{xml,properties} -rwl "${domain_home}/config/fmwconfig/" -e "$on_prem_jdbc_string" | xargs sed -i "s|$on_prem_jdbc_string|$connection_url|g");
     exit_code=$?
     echo "Executed datasource ATP update on jps-config*.xml with exit code [$exit_code]" | log >> $log_file
@@ -108,20 +103,6 @@ cd "${domain_home}/config/jdbc" || (echo "Failed to cd to ${domain_home}/config/
     fi
 
   elif [[ $is_atp == "false" ]] && [[ $is_oci_db == "true" ]]; then
-
-
-    #Opening port 1522 in the subnet of the selected Autonomous Transaction Processing (ATP) Database, if the checkbox is checked.
-    if [ "$is_admin_instance" = "true" ] && [ "${jdbc_string.oci_db_create_ingress_sl}" = "true" ]; then
-        output=$(python3 /opt/scripts/open_db_port.py "${jdbc_string.oci_port}" "${jdbc_string.oci_network_comp}" "${jdbc_string.oci_vcn_id}" "${jdbc_string.oci_subnet_id}" 2>&1)
-        exit_code=$?
-        echo "Executed script to open ingress port ${jdbc_string.oci_port} in db subnet ${jdbc_string.oci_subnet_id} with exit code [$exit_code]" | log >> $log_file
-        echo "$output" | log >> $log_file
-        if [ $exit_code -ne 0 ]; then
-            echo "Error executing the script to open ingress port ${jdbc_string.oci_port} in db subnet ${jdbc_string.oci_subnet_id}" | log >> $log_file
-            exit 1
-        fi
-    fi
-
     files=$(grep -il "$on_prem_jdbc_string" "${domain_home}/config/jdbc/"*.xml)
     for file in $files; do
         output=$(python3 /opt/scripts/ds_update_config_xml_w_db_system.py "$file" "$oci_jdbc_string")
@@ -167,4 +148,3 @@ cd "${domain_home}/config/jdbc" || (echo "Failed to cd to ${domain_home}/config/
   fi
 
 %{ endfor ~}
-
